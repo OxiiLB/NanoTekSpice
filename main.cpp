cb64c6bf067c117e4605de7aa12677bcc6151826
OxiiLB <romain.giraud@epitech.eu>
2024-02-26 14:11:02 +0100

    [FIX] BS

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(void)
     std::unique_ptr<nts::IComponent>and_gate = std::make_unique<nts::AndComponent>();
     std::unique_ptr<nts::IComponent>inverter = std::make_unique<nts::NotComponent>();
     and_gate->setLink(1, *input1, 1);
-    and_gate->setLink(2, *input2, 1);
+    and_gate->setLink(2, *input2, 1)
     inverter->setLink(1 , *and_gate , 3);
     std::cout << "!(" << input1->compute(1) << " && " << input2->compute(1) << ") ->" << inverter->compute(2) << std::endl;
 }

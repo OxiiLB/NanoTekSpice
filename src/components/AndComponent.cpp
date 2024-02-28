8b1f02222ade35ce1659220f3308c84caf182e9e
OxiiLB <romain.giraud@epitech.eu>
2024-02-28 20:23:01 +0100

    [FIX] Undefined on components

--- src/components/AndComponent.cpp
+++ src/components/AndComponent.cpp
@@ -19,12 +19,12 @@ nts::AndComponent::~AndComponent()
 nts::Tristate nts::AndComponent::compute(std::size_t pin)
 {
     if (pin == 3) {
-        if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::True &&
-        this->_links[1].second->compute(this->_links[1].first) == nts::Tristate::True)
-            return nts::Tristate::True;
-        else if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::False ||
+        if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::False ||
         this->_links[1].second->compute(this->_links[1].first) == nts::Tristate::False)
             return nts::Tristate::False;
+        else if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::True &&
+        this->_links[1].second->compute(this->_links[1].first) == nts::Tristate::True)
+            return nts::Tristate::True;
         else
             return nts::Tristate::Undefined;
     }
--- src/components/XorComponent.cpp
+++ src/components/XorComponent.cpp
@@ -19,20 +19,14 @@ nts::XorComponent::~XorComponent()
 nts::Tristate nts::XorComponent::compute(std::size_t pin)
 {
     if (pin == 3) {
-        if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::True &&
-        this->_links[1].second->compute(this->_links[1].first) == nts::Tristate::False)
-            return nts::Tristate::True;
-        else if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::False &&
-        this->_links[1].second->compute(this->_links[1].first) == nts::Tristate::True)
-            return nts::Tristate::True;
-        else if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::True &&
-        this->_links[1].second->compute(this->_links[1].first) == nts::Tristate::True)
-            return nts::Tristate::False;
-        else if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::False &&
-        this->_links[1].second->compute(this->_links[1].first) == nts::Tristate::False)
+        if (this->_links[0].second->compute(this->_links[0].first) == nts::Tristate::Undefined ||
+        this->_links[1].second->compute(this->_links[1].first) == nts::Tristate::Undefined)
+            return nts::Tristate::Undefined;
+        else if (this->_links[0].second->compute(this->_links[0].first) ==
+        this->_links[1].second->compute(this->_links[1].first))
             return nts::Tristate::False;
         else
-            return nts::Tristate::Undefined;
+            return nts::Tristate::True;
     }
     throw nts::PinException();
 }

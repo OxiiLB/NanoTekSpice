f691c55725872cc30e304e560cdaf6f4ff5c2436
alea chlodnik <achlodnik@gmail.com>
2024-03-01 18:03:50 +0100

    [UPD] added the rest or the gates to comp_type_array

--- Parser/Parser.hpp
+++ Parser/Parser.hpp
@@ -16,18 +16,18 @@
     #include <iostream>
     #include "../includes/Circuit.hpp"
 
-    static const int array_size = 11;
+    static const int array_size = 15;
 
     static char comp_type_name_array[array_size][10] = {
-        "clock", "and", "xor", "or", "not",
-        "input", "output", "false", "true",
-        "4001", "4011"
+        "clock", "and", "xor", "or", "not", "input",
+        "output", "false", "true", "4001", "4011",
+        "4030", "4069", "4071", "4081"
     };
 
     static char comp_type_array[array_size][10] = {
-        "clock", "and", "xor", "or", "not",
-        "input", "output", "false", "true",
-        "nor", "nand"
+        "clock", "and", "xor", "or", "not", "input",
+        "output", "false", "true", "nor", "nand",
+        "xor4", "inverter", "or4", "and4"
     };
 
 void check_config_file(const std::string &file_name);
